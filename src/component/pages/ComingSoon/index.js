9f56121cf2ff5c003309451a1ef3a12b7e89e36d
blacksofRahul <rahul.blacksof@gmail.com>
2022-01-16 14:17:06 +0530

    first commit

--- src/component/pages/ComingSoon/index.js
+++ src/component/pages/ComingSoon/index.js
@@ -4,22 +4,36 @@ import './style.css'
 export const ComingSoon = () => {
     return (
         <div className="coming_soon">
-            <div>
-                <img src={'./images/comingsoon/Cat.svg'} style={{ width: '7%', marginRight: '2%', float: 'right' }} alt='Cat' />
+            <div className='kiro'>
+                <img className='kiro_img' src={'./images/comingsoon/Cat.svg'} alt='kiro' />
+            </div>
+
+            <div className='coming_soon_text'>
+                <h1 className='coming_soon_dude'>COMING SOON</h1>
+                <h1 className='middle_text'>“We are preparing something exciting
+                    & amazing for you”</h1>
+                <img src={'./images/comingsoon/logo.svg'} className='logo_dude_to_doodle' alt='dude_to_doodle_logo' />
             </div>
 
             <div>
-                <h1 style={{ fontFamily: 'outfit' }}>COMING SOON</h1>
-                <h1>"We are preparing something exciting & amazing for you"</h1>
+                <div>
+                    <img src={'./images/comingsoon/Dog.svg'} className='lucy_img' alt="Dog" />
+                </div>
+                <div className='social_details'>
+                    <div>
+                        <h2 className='font_change'>Contact us : hello@dudetodoodle.in</h2>
+                    </div>
+                    <div className='social_icon'>
+                        <a href='https://www.behance.net/dudetodoodle' target='_blank'><img src={'./images/comingsoon/Behance.png'} className='social_media_icon1' alt="Dog" /></a>
+                        <a href='https://www.linkedin.com/company/dudetodoodle/' target='_blank'><img src={'./images/comingsoon/linkedin.png'} className='social_media_icon2' alt="Dog" /></a>
+                        <a href='https://www.instagram.com/dude_to_doodle/' target='_blank'><img src={'./images/comingsoon/instagram.png'} className='social_media_icon3' alt="Dog" /></a>
+                    </div>
+                    <div>
+                        <h2 className='font_change'>&copy;dudetodoodle studio di design creativo 2022</h2>
+                    </div>
+                </div>
             </div>
 
-            <div>
-                <img src={'./images/comingsoon/Dog.svg'} style={{ width: '10%' }} />
-            </div>
-
-            <div>
-                <h1 style={{ fontFamily: 'outfit' }}>Contact : hello@dudetodoodle.in</h1>
-            </div>
 
         </div>
     )
